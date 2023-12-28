7e04b024688736d5f95fc08d4adfdbe1cb075c69
PendingUsername <cook.joshual@yahoo.com>
2023-12-27 22:23:26 -0500

    docker app

--- application.py
+++ application.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def calculator():
-	if request.method == 'POST':
-		num1 = request.form['num1']
-		num2 = request.form['num2']
-		operation = request.form['operation']
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
 
-		if operation == 'add':
-			result = int(num1) + int(num2)
-		elif operation == 'subtract':
-			result = int(num1) - int(num2)
-		elif operation == 'multiply':
-			result = int(num1) * int(num2)
-		elif operation == 'divide':
-			result = int(num1) / int(num2)
+        if operation == 'add':
+            result = int(num1) + int(num2)
+        elif operation == 'subtract':
+            result = int(num1) - int(num2)
+        elif operation == 'multiply':
+            result = int(num1) * int(num2)
+        elif operation == 'divide':
+            result = int(num1) / int(num2)
 
-		return render_template('calculator.html', result=result)
+        return render_template('calculator.html', result=result)
 
-	return render_template('calculator.html')
+    return render_template('calculator.html')
 
 if __name__ == '__main__':
-	application.run(debug=True)
+    # Use '0.0.0.0' to make the app accessible externally
+    app.run(debug=True, host='0.0.0.0')
